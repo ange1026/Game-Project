faafc495bd70194b82dcc0ebe9d7840657adff70
angie10 <delacruzvargasangelica26@gmail.com>
2022-09-25 10:36:57 -0400

    second update

--- js/index.js
+++ js/index.js
@@ -1,24 +1,63 @@
-document.addEventListener('DOMContentLoaded', () => {
-const rToken = document.querySelector('.redtoken');
-const bToken = document.querySelector('.bluetoken');
-const goBackOne = document.querySelector('#gobacktothestart1');
-const playerDisplay = document.querySelector('.display-player');
 const allTiles = document.querySelectorAll('.tile');
-const gameBoard = document.querySelector('.board');
-const announcer = document.querySelector('.announcer');
+const start = document.getElementById('start');
+const players = document.querySelectorAll('.player');
 const restartBtn = document.querySelector('#restart');
 
-// Make the tokens clickable
-allTiles.forEach((tile) => {
-    tile.addEventListener('click')
+// Make the tiles clickable
+allTiles.forEach(tile => {
+    tile.addEventListener('click', play);
 })
 
-// rToken.addEventListener('click', getToken )
+let currentToken =  null;
 
+function play(e) {
+    const clickedTile = e.target;
+    console.log('The tile has been clicked!! ', clickedTile);
+    console.log('You should move ', 
+        currentToken && currentToken.className, 
+        ' to this current tile which is ', clickedTile.textContent)
+    //   Check if the tile clicked is a color token
+        const isToken = ['redtoken', 'bluetoken'].includes(clickedTile.className);
+        if (isToken) {
+            currentToken = clickedTile;
+            // Change current player
+            const color = currentToken.getAttribute('data-color');
+            players.forEach(player => {
+                // remove the class playing from all players
+                player.classList.remove('playing');
+                // Check if the current token color corresponds to the player, if it is add back the playing class.
+                if (player.classList.contains(color)) {
+                    player.classList.add('playing');
+                }
+            });
 
+            return;
+        }
+        
+        // Check if a color token has been clicked before, otherwise exits
+        if (!currentToken) return;
 
+        // Check if the tile clicked has an action.
+        const hasToJump = clickedTile.hasAttribute('data-target');
+        if (hasToJump) {
+            const whereToJump = clickedTile.getAttribute('data-target');
+            console.log(whereToJump);
+            document.getElementById(`tile${whereToJump}`).appendChild(currentToken);
+            return;
+        }
 
-});
+        // Go back to start tile.
+     const isGoBacktoStart = clickedTile.classList.contains("backtothestart");
+      if (isGoBacktoStart) {
+        start.appendChild(currentToken);
+        return;
+      }
+      
+        // It's a number tile.
+      clickedTile.appendChild(currentToken)
+}
+
+   
 
  //  Create a random Number from 1-6
  const randomNumber = () => {
